828c1353c0a37399fd5782b6f8048c09368bcfba
Adam Davis <davisadamw@gmail.com>
2019-07-15 11:47:55 -0700

    basic script to test process of combining adjacent rasters

---
+++ 01_01_dem-mosaic-tests.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+# identify source directory
+src_dir <- 'test_data'
+
+# unzip all the rasters in source
+map(list.files(src_dir, full.names=T), unzip, exdir=src_dir)
+
+# list all .img files (filenames ending in img) in directory, load them as rasters
+test_rasters <- map(list.files(src_dir, 'img$', full.names = T), raster::raster)
+
+# combine all the rasters into one place 
+# ... this is a lil time consuming with two, might take longer with more files
+# ... might be worth considering writing a function to selectively match to appropriate raster
+test_rasters_merged <- do.call(raster::merge, test_rasters)
